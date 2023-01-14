1fe2ae5da7d77ba4f2ad1de90fb85db57a86ca7e
Andrei.Verilov <Andrei.Verilov@urfu.me>
2023-01-14 19:23:01 +0500

    5

--- app1/models.py
+++ app1/models.py
@@ -4,6 +4,7 @@ class Profession (models.Model):
     title = models.CharField('Назввание',max_length=50)
     description = models.TextField('Описание')
 
+
     def __str__(self):
         return self.title
 
