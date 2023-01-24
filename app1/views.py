a7b334b7914bf39501014fbd064317690e294c80
Andrei.Verilov <Andrei.Verilov@urfu.me>
2023-01-24 20:53:46 +0500

    5

--- app1/views.py
+++ app1/views.py
@@ -5,4 +5,10 @@ def index_page(request):
     data = {
          'profession': Profession.objects.get(id=2)
     }
-    return render(request,"index.html",context=data)
+    return render(request, "index.html", context=data)
+
+def skills_page(request):
+    data = {
+        'search_url': 'ddd'
+    }
+    return render(request, "skills.html", context=data)
